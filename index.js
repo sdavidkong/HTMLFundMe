b68fdcdae6bca1be1750c8232cd6a6b60dd4c9f7
S David Kong <SDAVIDKONG@gmail.com>
2022-09-19 15:35:14 -0400

    added buttons

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import { ethers } from "./ethers-5.6.esm.min.js";
+
+const connectButton = document.getElementById("connectButton");
+
+const fundButton = document.getElementById("fund");
+
+connectButton.onclick = connect;
+fundButton.onclick = fund;
+
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
     await window.ethereum.request({ method: "eth_requestAccounts" });
