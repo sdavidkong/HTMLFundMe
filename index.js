7a28c0d465fcf894ee2553a884efef121f364f63
S David Kong <SDAVIDKONG@gmail.com>
2022-09-19 23:14:44 -0400

    added all buttons

--- index.js
+++ index.js
@@ -1,24 +1,79 @@
 import { ethers } from "./ethers-5.6.esm.min.js";
+import { abi, contractAddress } from "./constants.js";
 
 const connectButton = document.getElementById("connectButton");
 
 const fundButton = document.getElementById("fund");
+const balanceButton = document.getElementById("balanceButton");
+const withdrawButton = document.getElementById("withdrawButton");
 
 connectButton.onclick = connect;
 fundButton.onclick = fund;
+balanceButton.onclick = getBalance;
+withdrawButton.onclick = withdraw;
+console.log(ethers);
 
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
     await window.ethereum.request({ method: "eth_requestAccounts" });
-    document.getElementById("connectButton").innerHTML = "Connected!";
+    connectButton.innerHTML = "Connected!";
   } else {
-    document.getElementById("connectButton").innerHTML =
-      "Please install Metamask!";
+    connectButton.innerHTML = "Please install Metamask!";
+  }
+}
+async function getBalance() {
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const balance = await provider.getBalance(contractAddress);
+    console.log(ethers.utils.formatEther(balance));
   }
 }
 
-async function fund(ethAmount) {
+async function fund() {
+  const ethAmount = document.getElementById("ethAmount").value;
   console.log(`Funding with ${ethAmount} ETH...`);
   if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    console.log(signer);
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    try {
+      const transactionResponse = await contract.fund({
+        value: ethers.utils.parseEther(ethAmount),
+      });
+
+      await listenForTransActionMine(transactionResponse, provider);
+      console.log("Done!");
+    } catch (error) {
+      console.log(error);
+    }
   }
+}
+async function withdraw() {
+  if (typeof window.ethereum !== "undefined") {
+    console.log("Withdrawing...");
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const balance = await provider.getBalance(contractAddress);
+    const signer = provider.getSigner();
+    console.log(signer);
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    try {
+      const transactionResponse = await contract.withdraw();
+      await listenForTransActionMine(transactionResponse, provider);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+function listenForTransActionMine(transactionResponse, provider) {
+  console.log(`Mining ${transactionResponse.hash}...`);
+  return new Promise((resolve, reject) => {
+    provider.once(transactionResponse.hash, (TransactionReceipt) => {
+      console.log(
+        `Completed with ${TransactionReceipt.confirmations} confirmations`
+      );
+      resolve();
+    });
+  });
 }
